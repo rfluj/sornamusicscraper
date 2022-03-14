67dd996a6643daa4a41f8285a1fd2c026d873b34
rf_luj <aref.ra78@gmail.com>
2022-03-14 16:58:08 +0330

    v.0.1.2

--- SornaMusicScraper/bs.py
+++ SornaMusicScraper/bs.py
@@ -1,4 +1,5 @@
 
+from unittest import result
 from bs4 import BeautifulSoup
 
 class Bs:
@@ -31,14 +32,56 @@ class Bs:
             return False
 
     def getAllHomeArtists(self, response):
-        soup  = self.BeautifulSoup(response, "html.parser")
-        # print(soup.prettify())
-        atags = self.querySelector("aside.side-menu|ul|li.cat-item cat-item-2|ul.children|li|a", [soup], [])
-        if atags:
-            for a in atags:
-                obj = {'url' : a['href'], 'artist_name' : a.text}
-                print(obj)
-            print(len(atags))
-        else:
-            print('found nothing.')
+        try:
+            soup  = self.BeautifulSoup(response, "html.parser")
+            # print(soup.prettify())
+            atags = self.querySelector("aside.side-menu|ul|li.cat-item cat-item-2|ul.children|li|a", [soup], [])
+            if atags:
+                result = []
+                for a in atags:
+                    obj = {'url' : a['href'], 'artist_name' : a.text}
+                    result.append(obj)
+                return result
+            else:
+                return []
+        except:
+            return False
+    
+    # response --->>> result(array) and next page url if exist
+    def getArtistAllInfo(self, response):
+        try:
+            soup  = self.BeautifulSoup(response, "html.parser")
+            atags = self.querySelector("div.mycontent|article|header.clearfix|h2|a", [soup], [])
+            if atags:
+                result = []
+                for a in atags:
+                    url  = a['href']
+                    text = a.text
+                    if 'دانلود آلبوم' in text:
+                        type = 'album'
+                        name = text.split('دانلود آلبوم')[1]
+                    elif 'دانلود آهنگ' in text:
+                        type = 'song'
+                        name = text.split('دانلود آهنگ')[1]
+                    else:
+                        type = 'song'
+                        name = text
+                    obj = {
+                        'url' : url,
+                        'name' : name,
+                        'type' : type
+                    }
+                    result.append(obj)
+                nextpagetag = self.querySelector("div.navigation|ul|li|a", [soup], [])
+                nextpageurl = False  
+                if nextpagetag:
+                    for a in nextpagetag:
+                        if 'برگهٔ بعد »' in a.text:
+                            nextpageurl = a['href']
+                            break
+                return result, nextpageurl
+            else:
+                return False, False
+        except Exception as e:
+            return False, False
 
--- SornaMusicScraper/sornamusicscraper.py
+++ SornaMusicScraper/sornamusicscraper.py
@@ -1,4 +1,5 @@
 
+from webbrowser import get
 from .req import Req
 from .globals import Globals
 from .bs import Bs
@@ -10,10 +11,50 @@ class SornaMusicScraper:
         self.globals = Globals()
         self.bs      = Bs()
     
+    # create function for get all artists list
+    # input    --->>> None
+    # response --->>> result , message, statuscode
     def getAllArtists(self):
         statuscode, response = self.req.sendrequest(self.baseurl, headers=self.globals.HomePageHeader)
-        print(statuscode)
-        if statuscode == 200:
-            self.bs.getAllHomeArtists(response.text)
+        if statuscode == -1 or statuscode == -2:
+            return False, "error in send request.", -4
+        elif statuscode == 200:
+            result = self.bs.getAllHomeArtists(response.text)
+            if result:
+                if len(result) == 0:
+                    return False, "nothing found in page. something wrong.", -1
+                else:
+                    return result, "successful", 1
+            else:
+                return False, "error in scrap home page. url : " + self.baseurl, -2
+        else:
+            return False, "error in load page all artists. Status_Code : " + str(statuscode), -3
+    
+    # create function for get artist info. like songs and albums
+    # input    --->>> artist page url
+    # response --->>>
+    def getArtistAllInfo(self, url, output=[]):
+        statuscode, response = self.req.sendrequest(url, headers=self.globals.HomePageHeader)
+        if statuscode == -1 or statuscode == -2:
+            return False, "error in send request.", -4
+        elif statuscode == 200:
+            result, nextpage = self.bs.getArtistAllInfo(response.text)
+            if result:
+                output      += result
+                checkmessage = True
+                if nextpage:
+                    output, message, statuscode = self.getArtistAllInfo(nextpage, output)
+                    checkmessage = False
+                if statuscode == 1:
+                    message = "successful"
+                    checkmessage = False
+                if checkmessage:
+                    message = 'successful'
+                return output, message, statuscode
+            else:
+                return False, "error in scrap artist page. url : " + url, -2
+        else:
+            return False, "error in load page artist. Status_Code : " + str(statuscode), -3
+            
 
 
